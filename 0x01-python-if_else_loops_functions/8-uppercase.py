f4cd997b323899c4d5d5f89cc1805dce28d57b18
sofoniasgd <zephaniahsofa@gmail.com>
2023-11-28 22:32:31 +0000

    added task 8]

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+def uppercase(str):
+    newstr = ""
+    for c in str:
+        if (ord(c) >= 97 and ord(c) <= 122):
+            newstr += chr(ord(c) - 32)
+        else:
+            newstr += c
+    print("{}".format(newstr))
