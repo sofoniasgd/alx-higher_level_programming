c8d0c556709254cd4cd822e55d4bfc0f6f8f564a
sofoniasgd <zephaniahsofa@gmail.com>
2023-11-30 22:05:58 +0000

    added task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    args = len(argv) - 1
+    if args >= 1:
+        str1 = ":"
+    else:
+        str1 = "."
+    if args == 1:
+        str2 = "argument"
+    else:
+        str2 = "arguments"
+    print("{} {}".format(args, str2+str1))
+    for i in range(len(argv)):
+        print("{}: {}".format(i, argv[i]))
