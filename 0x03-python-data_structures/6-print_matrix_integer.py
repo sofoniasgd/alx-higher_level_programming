d780f43c1d283459ca149f5a31a2c5f6cfe3a5ab
sofoniasgd <zephaniahsofa@gmail.com>
2023-12-05 08:39:33 +0000

    added task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return None
+    for x in range(len(matrix)):
+        for y in range(len(matrix[x])):
+            if y != (len(matrix[x]) - 1):
+                print("{:d}".format(matrix[x][y]), end=' ')
+            else:
+                print("{:d}".format(matrix[x][y]))
