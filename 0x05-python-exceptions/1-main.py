d923c64dc551b4cc9af2b3d4bb7379122f601269
sofoniasgd <zephaniahsofa@gmail.com>
2023-12-18 23:15:09 +0000

    updated task 1

--- 0x05-python-exceptions/1-main.py
+++ 0x05-python-exceptions/1-main.py
@@ -6,6 +6,10 @@ has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
 
+value = [1,2,3,4]
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
 value = -89
 has_been_print = safe_print_integer(value)
 if not has_been_print:
