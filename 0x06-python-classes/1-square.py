e537a4e73caf9c6863d77783a2769693fa5d7022
sofoniasgd <zephaniahsofa@gmail.com>
2023-12-20 01:27:06 +0000

    added task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""square class module for task 0."""
+
+
+class Square:
+    """Define square class."""
+    def __init__(self, size):
+        """define private attriubte size.
+        Args:
+            size (int): size of a square
+        """
+        self.__size = size
