26fadfb143a3ddb6bc3acc969522fad7e28b9f8e
sofoniasgd <zephaniahsofa@gmail.com>
2024-01-05 23:00:53 +0000

    added task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""square class module for task 0."""
+
+
+class Square:
+    """Define square class."""
+    def __init__(self, size=0):
+        """define private attriubte size.
+        check size is an int and of less than zero value
+        then raises errors if not.
+        Args:
+            size (int): size of a square
+        """
+        if (not isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Define an area method to return area of the square"""
+        return self.__size * self.__size
