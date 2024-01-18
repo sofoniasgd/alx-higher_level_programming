17fff7b615615898fe5e1179b26a18208fc9156a
sofoniasgd <zephaniahsofa@gmail.com>
2024-01-18 02:21:09 +0000

    updated files for task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,11 +15,33 @@ class Rectangle:
         self.height = height
 
     @property
-    def width(self):
-        """width getter method"""
-        return self.__width
-    @property
     def height(self):
         """height getter method"""
         return self.__height
-    ##setters left
+
+    @property
+    def width(self):
+        """width getter method"""
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        """setter for height attribute
+        checks if value is an integer
+        """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    @width.setter
+    def width(self, value):
+        """setter for width attribute
+        checks if value is an integer
+        """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
