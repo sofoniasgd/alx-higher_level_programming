359a1d2640e9a78a2839c527e85c75ad7df3fcb2
sofoniasgd <zephaniahsofa@gmail.com>
2024-01-25 16:02:58 +0000

    updated files for task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    maxint = None
+    max_int = None
+    max_key = None
     if not a_dictionary:
         return None
     for key, value in a_dictionary.items():
-        if not maxint or value > maxint:
-            maxint = key
-    return maxint
+        if not max_int or value > max_int:
+            max_int = value
+            max_key = key
+    return key
