0fed41852a61c953f622ce328166adf98e9332b5
sofoniasgd <zephaniahsofa@gmail.com>
2024-02-05 12:34:29 +0000

    added files for task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" task 5 module. """
+
+
+class BaseGeometry():
+    """BaseGeometry class(does nothing)"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
