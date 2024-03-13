925b1a186237b49e4ed1863c89e766fd3847a4ff
sofoniasgd <zephaniahsofa@gmail.com>
2024-03-13 02:14:09 +0000

    added task 8

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,1 +1,3 @@
-insert into first_table values (89, "Best School");
+-- inserts a new row in the table first_table (database hbtn_0c_0)
+-- new row id->89, name="Bese School"
+INSERT INTO first_table VALUES (89, "Best School");
