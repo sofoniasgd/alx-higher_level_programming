ab026f9a93e1cab8598d0e418d5230dfef465d82
sofoniasgd <zephaniahsofa@gmail.com>
2024-03-25 15:08:58 +0000

    added task 9 and 10

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Task 9 module"""
+
+
+class Student():
+    """Defines a student"""
+
+
+    def __init__(self, first_name, last_name, age):
+        """defines a student object
+        Args:
+            first_name (str)
+            last_name (str)
+            age (str)
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieves a dictionary representation of a Student instance
+        """
+        return self.__dict__
