a620190a17ded0808effd27639e108875a6382f7
sofoniasgd <zephaniahsofa@gmail.com>
2024-04-08 11:07:37 +0000

    added task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 while (i < 3) {
   console.log(strings[i]);
