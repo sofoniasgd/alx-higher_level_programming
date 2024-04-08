af8a9b176f1b173a6b1c8a5e4b5fd10641f6fa55
sofoniasgd <zephaniahsofa@gmail.com>
2024-04-08 12:30:04 +0000

    added task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const commandArgs = process.argv;
+const firstArgument = Number(commandArgs[2]);
+let text = '';
+
+if (!firstArgument) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < firstArgument; i++) {
+    text += '#';
+  }
+  for (let j = 0; j < firstArgument; j++) {
+    console.log(text);
+  }
+}
