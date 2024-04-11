5fa4a9eba57e5a527561e5eb68fb22fbdf0b092e
sofoniasgd <zephaniahsofa@gmail.com>
2024-04-11 05:54:36 +0000

    updated task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,18 +4,19 @@ const SquareParent = require('./5-square');
 class Square extends SquareParent {
   constructor (size) {
     super(size);
-    this.size = size;
+    this.size = this.width;
   }
 
+  // USE "this.width || this.length" INSTEAD OF "this.size"
   charPrint (c) {
     if (!c) {
       super.print();
     } else {
       let row = '';
-      for (let i = 0; i < this.size; i++) {
+      for (let i = 0; i < this.width; i++) {
         row += c;
       }
-      for (let i = 0; i < this.size; i++) {
+      for (let i = 0; i < this.width; i++) {
         console.log(row);
       }
     }
