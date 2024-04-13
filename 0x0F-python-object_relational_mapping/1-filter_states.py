fe351e4c5b99be91ec4e539bf5352105b4eeeffd
sofoniasgd <zephaniahsofa@gmail.com>
2024-04-13 00:48:55 +0000

    added task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""GET ALL STATES(Task 0)"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # exit if arguments are not 3
+    if len(sys.argv) != 4:
+        sys.exit()
+    # store arguments into more readable names
+    username, password, dbname = sys.argv[1:4]
+    # connect to database
+    db = MySQLdb.connect(host='localhost', user=username, passwd=password,
+                         db=dbname, port=3306)
+    cur = db.cursor()
+    # execute and print query
+    cur.execute("SELECT * FROM states where name LIKE 'N%'")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
