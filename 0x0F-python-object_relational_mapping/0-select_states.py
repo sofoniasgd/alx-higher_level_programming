e522f35e5027297a44f0d4cab1aface848130ed6
sofoniasgd <zephaniahsofa@gmail.com>
2024-04-13 00:43:26 +0000

    updated task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,32 +8,16 @@ if __name__ == "__main__":
     # exit if arguments are not 3
     if len(sys.argv) != 4:
         sys.exit()
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbname = sys.argv[3]
+    # store arguments into more readable names
+    username, password, dbname = sys.argv[1:4]
     # connect to database
     db = MySQLdb.connect(host='localhost', user=username, passwd=password,
-                         port=3306)
-    try:
-        db.select_db(dbname)
-    except MySQLdb.OperationalError:
-        cur = db.cursor()
-        cur.execute(f"CREATE DATABASE IF NOT EXISTS {dbname}")
-        db.commit()
-        cur.close()
-        db.select_db(dbname)
-    # create the table
+                         db=dbname, port=3306)
     cur = db.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS states \
-            (id INT NOT NULL AUTO_INCREMENT, \
-            name VARCHAR(256) NOT NULL, \
-            PRIMARY KEY (id) )")
-    cur.execute("INSERT INTO states (name) VALUES \
-            ('California'), ('Arizona'), ('Texas'), \
-            ('New York'), ('Nevada')")
+    # execute and print query
     cur.execute("SELECT * FROM states ORDER BY id")
-    db.commit()
     rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
+    db.close()
