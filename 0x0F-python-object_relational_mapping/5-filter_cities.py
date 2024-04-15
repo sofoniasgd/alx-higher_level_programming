87281358b835d6b731e52f1b423bc4e0777f951f
sofoniasgd <zephaniahsofa@gmail.com>
2024-04-15 11:15:46 +0000

    updated task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""SEARCH ALL STATES WITH KEYWORD(Task 2)"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # store arguments into more readable names
+    username, password, dbname, state_name = sys.argv[1:5]
+    # connect to database
+    db = MySQLdb.connect(host='localhost', user=username, passwd=password,
+                         db=dbname, port=3306)
+    cur = db.cursor()
+    # execute and print query
+    cur.execute("SELECT GROUP_CONCAT(cities.name) FROM cities LEFT JOIN \
+                states ON state_id=states.id WHERE states.name='{}' ORDER BY \
+                cities.id".format(state_name))
+    rows = cur.fetchall()
+    for items in rows[0]:
+        print(items)
+    cur.close()
+    db.close()
