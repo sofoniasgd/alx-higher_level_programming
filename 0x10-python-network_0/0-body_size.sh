f855ddf2baa80c2eb3cff9252a5fdadd51c589b9
sofoniasgd <zephaniahsofa@gmail.com>
2024-04-25 23:34:21 +0000

    updated task 0&1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a request to that \
-# URL, and displays the size of the body of the response
-# 	-s silent mode
-# 	-o /dev/null discard the output cuz we're not interested
-# 	-w ${size_download} custom output format
+# a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -s -o /dev/null -w "%{size_download}\n" "$1"
