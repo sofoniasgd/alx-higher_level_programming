92f88ae8036e2aa66a2afeb76910b5172ef2d8cb
sofoniasgd <zephaniahsofa@gmail.com>
2024-04-26 19:19:46 -0700

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""4. What's my status? #1(Task 4)"""
+
+import requests
+
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
