0ed21f01576b89037ecb7df1af4dc8e04e956a0f
sofoniasgd <zephaniahsofa@gmail.com>
2024-06-06 02:40:17 +0000

    task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,7 +3,8 @@
 const args = process.argv;
 const request = require('request');
 
-let urlList = [];
+const urlList = [];
+
 const url = {
   url: 'https://swapi-api.alx-tools.com/api/films/' + args[2]
 };
@@ -17,18 +18,23 @@ request(url, (error, response, body) => {
   characters.forEach(function (url) {
     urlList.push(url);
   });
-  function sequentialReq(urlList, index = 0) {
-    if (index < characters.length) {
+
+  // define function that makes request sequential
+  function sequentialReq (urlList, index = 0) {
+    if (index < urlList.length) {
       request(urlList[index], (error, response, body) => {
-        if (error) {
+        if (!error && response.statusCode === 200) {
+          const character = JSON.parse(body);
+          // print name
+          console.log(character.name);
+          // next function call
+          sequentialReq(urlList, index + 1);
+        } else {
           return console.error(error);
         }
-	const character = JSON.parse(body);
-	console.log(character.name);
-	
-	sequentialReq(urlList, index + 1);
       });
     }
   }
-	console.log(urlList);
+  // make function call
+  sequentialReq(urlList);
 });
